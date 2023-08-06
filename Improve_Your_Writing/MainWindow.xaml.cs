b0cb342172d34b4cdab7f4c144c0ecf650e48e31
SHM-white <1131287310a@gmail.com>
2023-08-06 20:43:53 +0800

    添加项目文件。

---
+++ Improve_Your_Writing/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Improve_Your_Writing_Core;
+using Microsoft.Win32;
+
+namespace Improve_Your_Writing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DocumentSettings DocumentSettingsInstance { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DocumentSettingsInstance = new DocumentSettings()
+            {
+                // 初始化属性值
+                FontSize = 12,
+                FontName = "Arial",
+                OutputDocxPath = "output.docx",
+                InputXlsxPath = "input.xlsx",
+                StartAfterLine = 1
+            };
+            DataContext = DocumentSettingsInstance;
+        }
+        private void Button_ChooseXlsx_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Xlsx Document (*.xlsx)|*.xlsx";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                TextBox_XlsxPath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void Button_ChooseDocx_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Word Document (*.docx)|*.docx";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                TextBox_DocxPath.Text = saveFileDialog.FileName;
+            }
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Improve_Your_Writing_Core/Core.cs
@@ -0,0 +1,172 @@
+﻿using System.ComponentModel;
+using System.Runtime.Versioning;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.XWPF.UserModel;
+using NPOI.OpenXmlFormats.Wordprocessing;
+
+
+namespace Improve_Your_Writing_Core
+{
+    static public class Core
+    {
+        public static bool Run(DocumentSettings settings)
+        {
+            if (settings == null)
+            {
+                return false;
+            }
+            Dictionary<string, string> data = GetXlsxData(settings.InputXlsxPath);
+
+            return WriteToDocx(settings.OutputDocxPath, data, settings);
+        }
+
+        private static bool WriteToDocx(string path, Dictionary<string, string> data, DocumentSettings settings)
+        {
+            // 创建一个新的DOCX文档对象
+            XWPFDocument document = new XWPFDocument();
+
+            // 添加段落
+            XWPFParagraph paragraph = document.CreateParagraph();
+            XWPFRun run = paragraph.CreateRun();
+            run.SetText("Hello, World!");
+
+            // 保存文档
+            using (FileStream file = new FileStream(settings.OutputDocxPath, FileMode.Create, FileAccess.Write))
+            {
+                document.Write(file);
+            }
+
+            return true;
+        }
+
+        private static Dictionary<string, string> GetXlsxData(string path)
+        {
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            // 创建一个工作簿对象
+            IWorkbook workbook;
+            using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                workbook = new XSSFWorkbook(file);
+            }
+
+            // 获取第一个工作表
+            ISheet sheet = workbook.GetSheetAt(0);
+
+            // 遍历行
+            for (int i = 0; i <= sheet.LastRowNum; i++)
+            {
+                IRow row = sheet.GetRow(i);
+                if (row != null)
+                {
+                    string[] strings = new string[row.LastCellNum];
+
+                    // 遍历单元格
+                    for (int j = 0; j < row.LastCellNum; j++)
+                    {
+                        NPOI.SS.UserModel.ICell cell = row.GetCell(j);
+                        if (cell != null)
+                        {
+                            // 获取单元格的值
+                            string value = cell.ToString();
+                            // 处理单元格的值
+                            strings[j] = value;
+
+                        }
+                    }
+                    data.Add(strings[0], strings[1]);
+                }
+            }
+            return data;
+        }
+    }
+
+
+    public class DocumentSettings : INotifyPropertyChanged
+    {
+
+        public DocumentSettings()
+        {
+
+        }
+        private int _fontSize;
+        public int FontSize
+        {
+            get { return _fontSize; }
+            set
+            {
+                if (_fontSize != value)
+                {
+                    _fontSize = value;
+                    OnPropertyChanged(nameof(FontSize));
+                }
+            }
+        }
+
+        private string? _fontName;
+        public string? FontName
+        {
+            get { return _fontName; }
+            set
+            {
+                if (_fontName != value)
+                {
+                    _fontName = value;
+                    OnPropertyChanged(nameof(FontName));
+                }
+            }
+        }
+
+        private string? _outputDocxPath;
+        public string? OutputDocxPath
+        {
+            get { return _outputDocxPath; }
+            set
+            {
+                if (_outputDocxPath != value)
+                {
+                    _outputDocxPath = value;
+                    OnPropertyChanged(nameof(OutputDocxPath));
+                }
+            }
+        }
+
+        private string? _inputXlsxPath;
+        public string? InputXlsxPath
+        {
+            get { return _inputXlsxPath; }
+            set
+            {
+                if (_inputXlsxPath != value)
+                {
+                    _inputXlsxPath = value;
+                    OnPropertyChanged(nameof(InputXlsxPath));
+                }
+            }
+        }
+
+        private int _startAfterLine;
+        public int StartAfterLine
+        {
+            get { return _startAfterLine; }
+            set
+            {
+                if (_startAfterLine != value)
+                {
+                    _startAfterLine = value;
+                    OnPropertyChanged(nameof(StartAfterLine));
+                }
+            }
+        }
+
+        // INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
